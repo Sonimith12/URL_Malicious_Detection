dd333d6f7d5d0a5bf480237c85d022d2b50fd599
unknown <visalkao21@gmail.com>
2024-02-20 02:28:19 +0700

    ANN Updated

--- mlp.py
+++ mlp.py
@@ -8,7 +8,7 @@ from sklearn.model_selection import train_test_split
 
 
 
-data = pd.read_csv('D:/Files/IMT-Mines-Ales/S8/Introduction a Apprentissage Auto/Project/malicious_phish/malicious_phish.csv')
+data = pd.read_csv('../Dataset/malicious_phish.csv')
 
 # data = pd.read_csv('malicious_phish.csv')
 data.head(20)
